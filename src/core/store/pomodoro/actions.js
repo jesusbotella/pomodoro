4e47b713ad64892890a12286120dbe848d5501b7
Jesús Botella <jesus.botella@gmail.com>
2019-07-14 19:51:22 +0200

    Create actions and mutations for CRUD operations in pomodoros

--- src/core/store/pomodoro/actions.js
+++ src/core/store/pomodoro/actions.js
@@ -1,8 +1,41 @@
 /* eslint-disable */
+import ApiService from '@/core/services/api-service';
 import PomodoroModel from '@/core/models/pomodoro';
+import PomodoroService from '@/core/services/pomodoro-service';
+
+const pomodoroService = new PomodoroService(ApiService);
 
 export function createPomodoro(context, properties) {
   const pomodoro = new PomodoroModel(properties);
-  context.commit('addPomodoro', pomodoro);
+
+  return pomodoroService.create(pomodoro)
+    .then(() => {
+      context.commit('addPomodoro', pomodoro);
+    })
+    .catch(error => {
+      console.log('Something wrong happened!', error);
+    });
 }
 
+export function updatePomodoro(context, properties) {
+  return pomodoroService.update(
+      properties.id,
+      properties.propertiesToUpdate
+    )
+    .then(() => {
+      context.commit('updatePomodoro', properties);
+    })
+    .catch(error => {
+      console.log('Something wrong happened!', error);
+    });
+}
+
+export function deletePomodoro(context, pomodoroId) {
+  return pomodoroService.delete(pomodoroId)
+    .then(() => {
+      context.commit('deletePomodoro', pomodoroId);
+    })
+    .catch(error => {
+      console.log('Something wrong happened!', error);
+    });
+}
--- src/core/store/pomodoro/mutations.js
+++ src/core/store/pomodoro/mutations.js
@@ -1,3 +1,5 @@
+import Vue from 'vue';
+
 /* eslint-disable no-param-reassign */
 export function addPomodoro(state, pomodoro) {
   state.pomodoros = {
@@ -6,6 +8,16 @@ export function addPomodoro(state, pomodoro) {
   };
 }
 
-export function deletePomodoro() {
+export function updatePomodoro(state, pomodoroProperties) {
+  const pomodoro = state.pomodoros[pomodoroProperties.id];
 
+  Object
+    .keys(pomodoroProperties.propertiesToUpdate)
+    .forEach((property) => {
+      pomodoro.set(property, pomodoroProperties.propertiesToUpdate[property]);
+    });
+}
+
+export function deletePomodoro(state, pomodoroId) {
+  Vue.delete(state.pomodoros, pomodoroId);
 }
